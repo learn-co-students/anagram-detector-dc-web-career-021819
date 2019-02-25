375c08a140f3ac22959d92d4a9832ea79bd313b9
seanlucius <seanlucius22@icloud.com>
2019-02-24 22:45:31 -0500

    anagram-done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possibles)
+    possibles.select do |possible_match|
+      possible_match.split("").sort == self.word.split("").sort
+    end
+  end
+
+end
